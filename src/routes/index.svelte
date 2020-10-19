db79154f4eeabaa41c0bc58660bde811bcbc0e90
GenevieveAustin <GenevieveAustin@users.noreply.github.com>
2020-10-20 12:27:30 +1300

    database added
    Known bugs:
    Redirects to output even when undefined

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,78 @@
 <script>
+  // create an empty person object
+  let person = {
+    firstName: "",
+    lastName: "",
+    age: ""
+  };
+
+  let people = [];
+
+  function savePerson() {
+    console.log("savePerson() clicked");
+    db.collection("people")
+      .doc(person.firstName)
+      .set(person);
+  }
+
+  async function getPerson() {
+    console.log("getPerson() clicked");
+    let personDoc = await db
+      .collection("people")
+      .doc(person.firstName)
+      .get();
+    person = personDoc.data();
+  }
+
+  async function getPeople() {
+    console.log("getPeople() clicked");
+    let peopleCol = await db.collection("people").get();
+
+    /*
+    for (const personDoc in peopleCol) {
+        people.push(personDoc.data())
+    }*/
+
+    peopleCol.forEach(personDoc => {
+      people = [...people, personDoc.data()];
+    });
+  }
 </script>
 
-<style>
-</style>
+<!-- this is just a section to make the page look nice -->
+<section class="content section">
 
-<h1 class="title">Sapper</h1>
+  <h1>People</h1>
 
-<p>Welcome to coding with Svelte and Sapper!</p>
+  <!-- a place to enter the first name -->
+  <label>
+    First name:
+    <input bind:value={person.firstName} />
+  </label>
 
+  <!-- a place to enter the last name -->
+  <label>
+    Last name:
+    <input bind:value={person.lastName} />
+  </label>
+
+  <!-- a place to enter the age -->
+  <label>
+    Age:
+    <input bind:value={person.age} />
+  </label>
+
+  <button on:click={getPerson}>Get Person</button>
+
+  <button on:click={savePerson}>Save Person</button>
+
+  <button on:click={getPeople}>People</button>
+
+  {#each people as person}
+    {person.firstName}
+    {person.lastName}
+    {person.age}
+     ,  
+  {/each}
+
+</section>
